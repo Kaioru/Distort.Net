911b86558bb4d892f07d59475f10022e4fdd3af2
Kaioru <keifuurawrs@gmail.com>
2017-04-11 09:55:41 +0800

    Add Discore support

---
+++ Distort.Discore/DiscoreCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Discore.WebSocket;
+using Retort.Core;
+
+namespace Distort.Discore
+{
+	public abstract class DiscoreCommand : Command, DiscoreCommandExecutable
+	{
+		
+		public void Execute(List<string> args, MessageEventArgs e)
+		{
+			return;
+		}
+
+	}
+}
---
+++ Distort.Discore/DiscoreCommandExecutable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Discore.WebSocket;
+using Retort.Core;
+
+namespace Distort.Discore
+{
+	public interface DiscoreCommandExecutable : ICommandExecutable
+	{
+
+		void Execute(List<String> args, MessageEventArgs e);
+
+	}
+}
